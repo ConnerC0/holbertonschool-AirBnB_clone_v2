afad4a52d6b6cfa04d47c869421a1131912a907d
ConnerC0 <conner.curtis72@gmail.com>
2022-11-17 11:06:10 -0800

    stuff

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -8,8 +8,10 @@ from models.state import State
 app = Flask(__name__)
 
 
+
 @app.route("/states/<id>", strict_slashes=False)
 def state_id(id=None):
+    """displays 'Hello HBNB!'"""
     states = [states for states in storage.all(State).values()]
     return_state = None
     if id:
@@ -21,17 +23,21 @@ def state_id(id=None):
 
 @app.route("/cities_by_states", strict_slashes=False)
 def states_city_list():
+    """displays 'Hello HBNB!'"""
     states = storage.all(State)
     return render_template('8-cities_by_states.html', states=states)
 
 
 @app.route("/states/", strict_slashes=False)
 def states_list():
+    """displays 'Hello HBNB!'"""
+    states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
 def teardown(cont):
+    """remove the current sql alchemy session"""
     storage.close()
 
 
